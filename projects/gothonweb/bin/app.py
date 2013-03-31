3045a36805cdf1e67071783265f342aa167a4bad
AlisterTT <gengxin5213@gmail.com>
2013-03-31 14:15:55 +0800

    Mar.31

--- projects/gothonweb/bin/app.py
+++ projects/gothonweb/bin/app.py
@@ -2,18 +2,20 @@
 import web
 
 urls = (
-	'/hello', 'index'
+	'/hello', 'Index'
 )
 
 app = web.application(urls, globals())
 
-render = web.template.render('templates/')		# render 作为渲染用
+render = web.template.render('templates/', base="layout")		# render 作为渲染用
 
-class index(object):
+class Index(object):
 	def GET(self):
-		form = web.input(name="Nobody")
-		greeting = "Hello, %s" % form.name
+		return render.hello_form()
 
+	def POST(self):
+		form = web.input(name="Nobody", greet="Hello")
+		greeting = "%s, %s" % (form.greet, form.name)		
 		return render.index(greeting = greeting)	# 触发index.GET之后，返回render.index，并将greeting作为变量传递
 
 if __name__ == "__main__":
